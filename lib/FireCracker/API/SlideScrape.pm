1a6f6db8c85545b1b285195022d62a9e9a61b462
ytnobody <ytnobody@gmail.com>
2012-04-27 03:39:31 +0900

    slideshareのデザインレイアウトが変わったのでスクレーパも修正

--- lib/FireCracker/API/SlideScrape.pm
+++ lib/FireCracker/API/SlideScrape.pm
@@ -6,10 +6,10 @@ sub new {
     bless sub {
         my ( $content ) = @_;
         my $data = {};
-        ( $data->{doc} ) = $content =~ /<link href=".+\?doc=(.+)&amp;.+" name="media_presentation" rel="media:presentation" \/>/;
-        ( $data->{title} ) = $content =~ /<meta name="title" property="media:title" content="(.+)" \/>/;
-        ( $data->{description} ) = $content =~ /<meta name="description" property="dc:description" content="(.+)" \/>/;
-        ( $data->{author} ) = $content =~ /<meta name="dc_creator" property="dc:creator" content="(.+)" \/>/;
+        ( $data->{doc} ) = $content =~ /<meta name="og_video" property="og:video" content="http:\/\/static\.slidesharecdn\.com\/swf\/ssplayer2\.swf\?doc=(.+?)\&amp;.+" \/>/;
+        ( $data->{title} ) = $content =~ /<meta name="og_title" property="og:title" content="(.+)" \/>/;
+        ( $data->{description} ) = $content =~ /<meta name="og_description" property="og:description" content="(.+)" \/>/;
+        ( $data->{author} ) = $content =~ /<a href=".+" class="j-tooltip h-author-name notranslate" title=".+">(.+)<\/a>/;
         return $data;
     }, $class;
 }

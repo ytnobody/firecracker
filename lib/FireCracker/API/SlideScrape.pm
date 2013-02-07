8f2537813eba115923a19b326b295ed4c0e39639
ytnobody <ytnobody@gmail.com>
2013-02-07 17:03:28 +0900

    長らくスライドのロードに失敗していたのを解消(slideshareのタグが若干かわっとった)

--- lib/FireCracker/API/SlideScrape.pm
+++ lib/FireCracker/API/SlideScrape.pm
@@ -6,9 +6,9 @@ sub new {
     bless sub {
         my ( $content ) = @_;
         my $data = {};
-        ( $data->{doc} ) = $content =~ /<meta name="og_video" property="og:video" content="http:\/\/static\.slidesharecdn\.com\/swf\/ssplayer2\.swf\?doc=(.+?)\&amp;.+" \/>/;
-        ( $data->{title} ) = $content =~ /<meta name="og_title" property="og:title" content="(.+)" \/>/;
-        ( $data->{description} ) = $content =~ /<meta name="og_description" property="og:description" content="(.+)" \/>/;
+        ( $data->{doc} ) = $content =~ /<meta name="og_image" property="og:image" .* content="http:\/\/image.slidesharecdn.com\/(.+?)\/.*" \/>/;
+        ( $data->{title} ) = $content =~ /<meta name="og_title" property="og:title" .* content="(.+)" \/>/;
+        ( $data->{description} ) = $content =~ /<meta name="og_description" property="og:description" .* content="(.+)" \/>/;
         ( $data->{author} ) = $content =~ /<a href=".+" class="j-tooltip h-author-name notranslate" title=".+">(.+)<\/a>/;
         return $data;
     }, $class;
